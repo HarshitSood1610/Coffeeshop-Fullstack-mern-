5075e328341117005d6132c1aa8ad53941de4d9f
HarshitSood1610 <98547273+HarshitSood1610@users.noreply.github.com>
2024-05-28 00:33:46 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { AllRoutes } from './Components/AllRoutes'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <AllRoutes/>
+      </div>
+    </>
+  )
+}
+
+export default App
